e39a8678ea721fcff1876d71b916832e40bcf733
alexi-o <ohearnalexi@gmail.com>
2024-01-30 23:18:44 -0700

    sets up initial frontend to use flask app endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [metadata, setMetadata] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("photo", file);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:5000/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      const { image_url, metadata } = response.data;
+      setImageURL(`http://127.0.0.1:5000${image_url}`);
+      setMetadata(JSON.parse(metadata)); // Convert metadata string to JSON
+    } catch (error) {
+      console.error("Error uploading the file:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Upload and Metadata Generation</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload and Generate Metadata</button>
+
+      {imageURL && (
+        <img className="uploaded-image" src={imageURL} alt="Uploaded Image" />
+      )}
+
+      {metadata && (
+        <div className="metadata">
+          <h2>Generated Metadata:</h2>
+          <ul>
+            {metadata.map((prediction, index) => (
+              <li key={index}>
+                {prediction[1]} ({(prediction[2] * 100).toFixed(2)}%)
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
