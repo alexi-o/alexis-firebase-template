580e27c8906e4f3ba7158316979f896cb4a14f0d
alexi-o <ohearnalexi@gmail.com>
2024-02-01 23:12:58 -0700

    fixes reactivity with uploads and starts changing sidebar to a gallery

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button, Container, Grid, Input, Typography } from "@mui/material";
 import { CloudUpload } from "@mui/icons-material";
@@ -6,37 +6,60 @@ import Sidebar from "./Sidebar";
 import "./App.css";
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState([]);
   const [selectedUpload, setSelectedUpload] = useState(null);
   const [uploads, setUploads] = useState([]);
   const [keywords, setKeywords] = useState([]);
-  const [imageURL, setImageURL] = useState(null);
-  const [metadata, setMetadata] = useState(null);
+
+  useEffect(() => {
+    if (selectedUpload === null && uploads.length > 0) {
+      setSelectedUpload(uploads[0]);
+    }
+  }, [uploads, selectedUpload]);
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    setFiles([...files, ...e.target.files]);
   };
 
-  const handleUploadSuccess = (image_url, metadata) => {
-    const uploadData = {
-      imageUrl: `http://127.0.0.1:5000${image_url}`,
-      metadata: JSON.parse(metadata),
-    };
-    setUploads([...uploads, uploadData]);
-    setSelectedUpload(uploadData);
-    setImageURL(uploadData.imageUrl);
-    setMetadata(uploadData.metadata);
+  const handleBulkUpload = async () => {
+    const uploadPromises = files.map(async (file) => {
+      const formData = new FormData();
+      formData.append("photo", file);
+
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5000/upload",
+          formData
+        );
+
+        const { image_url, metadata } = response.data;
+
+        return {
+          name: generateRandomId(),
+          imageUrl: `http://127.0.0.1:5000${image_url}`,
+          metadata: JSON.parse(metadata),
+        };
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        return null;
+      }
+    });
+
+    const uploadedFiles = await Promise.all(uploadPromises);
+    setUploads((prevUploads) => [
+      ...prevUploads,
+      ...uploadedFiles.filter(Boolean),
+    ]);
+    setFiles([]);
   };
 
-  const handleUploadError = (error) => {
-    console.error("Error uploading the file:", error);
+  const generateRandomId = () => {
+    return Math.random().toString(36).substring(2, 15);
   };
 
   const selectUpload = (index) => {
     if (index >= 0 && index < uploads.length) {
       setSelectedUpload(uploads[index]);
-      setImageURL(uploads[index].imageUrl);
-      setMetadata(uploads[index].metadata);
     }
   };
 
@@ -53,28 +76,6 @@ function App() {
     setKeywords(updatedKeywords);
   };
 
-  const handleUpload = async () => {
-    const formData = new FormData();
-    formData.append("photo", file);
-
-    try {
-      const response = await axios.post(
-        "http://127.0.0.1:5000/upload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      const { image_url, metadata } = response.data;
-      handleUploadSuccess(image_url, metadata);
-    } catch (error) {
-      handleUploadError(error);
-    }
-  };
-
   return (
     <Container maxWidth="md" className="App">
       <Typography variant="h4" align="center" gutterBottom>
@@ -83,59 +84,72 @@ function App() {
       <Sidebar uploads={uploads} selectUpload={selectUpload} />
       <Grid container spacing={2} alignItems="center">
         <Grid item xs={12}>
-          <Input type="file" accept="image/*" onChange={handleFileChange} />
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+          />
         </Grid>
         <Grid item xs={12}>
           <Button
             variant="contained"
             color="primary"
             startIcon={<CloudUpload />}
-            onClick={handleUpload}
+            onClick={handleBulkUpload}
           >
-            Upload and Generate Metadata
+            Bulk Upload and Generate Metadata
           </Button>
         </Grid>
       </Grid>
-      {imageURL && (
-        <img className="uploaded-image" src={imageURL} alt="Uploaded Image" />
-      )}
-      {keywords.length > 0 && (
-        <div className="keyword-list">
-          <Typography variant="h6" gutterBottom>
-            Keywords:
-          </Typography>
-          <ul>
-            {keywords.map((keyword, index) => (
-              <li key={index}>
-                {keyword}{" "}
-                <button
-                  className="remove-button"
-                  onClick={() => removeKeyword(keyword)}
-                >
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      {metadata && (
-        <div className="metadata">
-          <Typography variant="h6" gutterBottom>
-            Generated Metadata:
-          </Typography>
-          <ul>
-            {metadata.map((prediction, index) => (
-              <li key={index}>
-                <button
-                  className="metadata-button"
-                  onClick={() => addKeyword(prediction[1])}
-                >
-                  {prediction[1]} ({(prediction[2] * 100).toFixed(2)}%)
-                </button>
-              </li>
-            ))}
-          </ul>
+      {selectedUpload && (
+        <div>
+          {selectedUpload.imageUrl && (
+            <img
+              className="uploaded-image"
+              src={selectedUpload.imageUrl}
+              alt="Uploaded file"
+            />
+          )}
+          {keywords.length > 0 && (
+            <div className="keyword-list">
+              <Typography variant="h6" gutterBottom>
+                Keywords:
+              </Typography>
+              <ul>
+                {keywords.map((keyword, index) => (
+                  <li key={index}>
+                    {keyword}{" "}
+                    <button
+                      className="remove-button"
+                      onClick={() => removeKeyword(keyword)}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {selectedUpload.metadata && (
+            <div className="metadata">
+              <Typography variant="h6" gutterBottom>
+                Generated Metadata:
+              </Typography>
+              <ul>
+                {selectedUpload.metadata.map((prediction, index) => (
+                  <li key={index}>
+                    <button
+                      className="metadata-button"
+                      onClick={() => addKeyword(prediction[1])}
+                    >
+                      {prediction[1]} ({(prediction[2] * 100).toFixed(2)}%)
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </Container>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Sidebar({ uploads, selectUpload }) {
+  const imageStyle = {
+    maxWidth: "100px", // Adjust the maximum width as needed
+    maxHeight: "100px", // Adjust the maximum height as needed
+  };
   return (
     <div className="sidebar">
       <h2>Previous Uploads</h2>
@@ -8,7 +12,15 @@ function Sidebar({ uploads, selectUpload }) {
         {uploads.map((upload, index) => (
           <li key={index}>
             <button onClick={() => selectUpload(index)}>
-              Upload {index + 1}
+              {upload.imageUrl ? (
+                <img
+                  src={upload.imageUrl}
+                  alt="Uploaded file"
+                  style={imageStyle}
+                />
+              ) : (
+                "No image"
+              )}
             </button>
           </li>
         ))}
