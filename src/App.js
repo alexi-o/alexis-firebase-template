e330ef6994478ebe8a57e3e2bbb72b020ac165fb
alexi-o <ohearnalexi@gmail.com>
2024-07-31 17:14:17 -0500

    minor update to remove navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Container, Typography, AppBar, Toolbar, Button } from "@mui/material";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "@mui/material";
 import MetadataExtraction from "./components/MetadataExtraction";
 
 function App() {
   return (
     <Router>
       <Container maxWidth="md" className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <Button color="inherit" component={Link} to="/">
-              Auto-tagger
-            </Button>
-            <Button color="inherit" component={Link} to="/metadata-extraction">
-              Metadata Extraction
-            </Button>
-          </Toolbar>
-        </AppBar>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/metadata-extraction" element={<MetadataExtraction />} />
+          <Route path="/" element={<MetadataExtraction />} />
         </Routes>
       </Container>
     </Router>
