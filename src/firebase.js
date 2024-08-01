8249093edb0f8b23fbd1a47cf6346f09f6f1ed55
alexi-o <ohearnalexi@gmail.com>
2024-08-01 16:26:18 -0500

    adds env file for keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBo4RiWk5IOJ8MZ05Dxm3hgTI7sQHAudHo",
-  authDomain: "meta-scraper-1bad5.firebaseapp.com",
-  databaseURL:
-    "https://meta-scraper-1bad5-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "meta-scraper-1bad5",
-  storageBucket: "meta-scraper-1bad5.appspot.com",
-  messagingSenderId: "57896844481",
-  appId: "1:57896844481:web:88b9eaa629fdece37e2840",
-  measurementId: "G-TW2457T0J1",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
