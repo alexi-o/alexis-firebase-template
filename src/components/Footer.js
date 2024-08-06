c6cbc8071addf5bdfb5ea84000c6aaad3d8455bc
alexi-o <ohearnalexi@gmail.com>
2024-08-06 00:58:34 -0500

    adds socials to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Typography, Link, useTheme } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   const theme = useTheme();
@@ -9,28 +11,68 @@ const Footer = () => {
       sx={{
         width: "100%",
         padding: "20px",
-        backgroundColor: theme.palette.background.default,
+        backgroundColor: theme.palette.background.paper,
         textAlign: "center",
-        borderTop: "1px solid #ccc",
+        borderTop: `1px solid ${theme.palette.primary.main}`,
         position: "fixed",
         bottom: 0,
         left: 0,
         right: 0,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Typography variant="body2" color="textSecondary">
-        <Link href="/about" underline="hover">
-          About Alexi
+      <Typography
+        variant="body2"
+        color={theme.palette.text.secondary}
+        sx={{ marginBottom: "10px" }}
+      >
+        <Link href="/about" underline="hover" color="inherit">
+          About
         </Link>{" "}
         |{" "}
-        <Link href="/contact" underline="hover">
-          Contact Alexi
+        <Link href="/contact" underline="hover" color="inherit">
+          Contact
         </Link>{" "}
         |{" "}
-        <Link href="/become-alexi" underline="hover">
-          Become Alexi
+        <Link href="/become-alexi" underline="hover" color="inherit">
+          Become
         </Link>
       </Typography>
+      <Box sx={{ display: "flex", gap: "10px" }}>
+        <Link
+          href="https://github.com/alexi-o"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            "&:hover": {
+              color: theme.palette.primary.main,
+            },
+          }}
+        >
+          <GitHubIcon />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/alexi-ohearn/"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            "&:hover": {
+              color: theme.palette.primary.main,
+            },
+          }}
+        >
+          <LinkedInIcon />
+        </Link>
+      </Box>
     </Box>
   );
 };
