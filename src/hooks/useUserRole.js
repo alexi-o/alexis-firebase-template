d7f60cc8c9a194f20fe8efcfe0664f6cf3a146f0
alexi-o <ohearnalexi@gmail.com>
2024-08-07 11:25:30 -0500

    update to userRole hook

--- src/hooks/useUserRole.js
+++ src/hooks/useUserRole.js
@@ -1,28 +1,34 @@
 import { useState, useEffect } from "react";
 import { auth, db } from "../firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 const useUserRole = () => {
   const [role, setRole] = useState(null);
 
   useEffect(() => {
-    const fetchUserRole = async () => {
-      if (auth.currentUser) {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
         try {
-          const userDoc = doc(db, "users", auth.currentUser.uid);
+          const userDoc = doc(db, "users", user.uid);
           const userSnap = await getDoc(userDoc);
 
           if (userSnap.exists()) {
             setRole(userSnap.data().role);
+          } else {
+            setRole(null);
           }
         } catch (error) {
           console.error("Error fetching user role:", error);
+          setRole(null);
         }
+      } else {
+        setRole(null);
       }
-    };
+    });
 
-    fetchUserRole();
-  }, [auth.currentUser]);
+    return () => unsubscribe();
+  }, []);
 
   return role;
 };
