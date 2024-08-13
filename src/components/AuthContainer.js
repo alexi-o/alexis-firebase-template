703da53053fdf72acce4e070664897e68ad31b0f
alexi-o <ohearnalexi@gmail.com>
2024-08-12 22:29:08 -0500

    removes title text from auth page

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import AuthTabs from "./AuthTabs";
-import { useTranslation } from "react-i18next";
 
 const AuthContainer = () => {
   const theme = useTheme();
-  const { t } = useTranslation();
 
   return (
     <Box
@@ -20,19 +18,6 @@ const AuthContainer = () => {
         paddingTop: "20px",
       }}
     >
-      <Typography
-        variant="h2"
-        sx={{
-          fontFamily: "'Press Start 2P', cursive",
-          color: theme.palette.primary.main,
-          textShadow: "2px 2px 4px #000000",
-          marginBottom: "30px",
-          textAlign: "center",
-          letterSpacing: "2px",
-        }}
-      >
-        {t("alexisWorld")}
-      </Typography>
       <AuthTabs />
     </Box>
   );
